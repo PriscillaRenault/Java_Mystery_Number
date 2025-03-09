9f1f60ad5ce945bf21709f32b71733d1b58fa3db
PriscillaRenault <priscilla.renault72460@gmail.com>
2025-03-09 18:24:02 +0100

    variable in method

--- src/Mystery_Number/Mystery_Number.java
+++ src/Mystery_Number/Mystery_Number.java
@@ -8,8 +8,15 @@ public class Mystery_Number {
 	}
 	
 	public static void playMysteryNumber() {
-		
+		 Scanner scanner = new Scanner(System.in);
+	        int MysteryNumber = (int) (Math.random() * 100) + 1;
+	        int round = 0;
+	        int test;
 
+	        System.out.println("Bienvenue dans le jeu du nombre mystère !");
+	        System.out.println("Devinez le nombre entre 1 et 100 :");
+
+	     
 	}
 
 }
