f64400d729e47d55160b7870e75031de541056f4
PriscillaRenault <priscilla.renault72460@gmail.com>
2025-03-09 18:25:25 +0100

    loop

--- src/Mystery_Number/Mystery_Number.java
+++ src/Mystery_Number/Mystery_Number.java
@@ -15,6 +15,21 @@ public class Mystery_Number {
 
 	        System.out.println("Bienvenue dans le jeu du nombre mystère !");
 	        System.out.println("Devinez le nombre entre 1 et 100 :");
+	        do {
+	            System.out.print("Votre proposition : ");
+	             test= scanner.nextInt();
+	            round++;
+
+	            if (test < MysteryNumber) {
+	                System.out.println("C'est plus !");
+	            } else if (test > MysteryNumber) {
+	                System.out.println("C'est moins !");
+	            } else {
+	                System.out.println("Bravo, vous avez trouvé le nombre mystère (" + MysteryNumber + ") en " + round + " essais !");
+	            }
+	        } while (test != MysteryNumber);
+
+	        scanner.close();
 
 	     
 	}
