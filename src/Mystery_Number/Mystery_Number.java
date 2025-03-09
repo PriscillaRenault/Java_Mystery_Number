475a298556bda0ed9c730868454197e6d997a2f8
PriscillaRenault <priscilla.renault72460@gmail.com>
2025-03-09 18:13:04 +0100

    initialise game with variable

---
+++ src/Mystery_Number/Mystery_Number.java
@@ -0,0 +1,18 @@
+package Mystery_Number;
+import java.util.Scanner;
+
+public class Mystery_Number {
+
+	public static void main(String[] args) {
+		 Scanner scanner = new Scanner(System.in);
+	        int MysteryNumber = (int) (Math.random() * 100) + 1;
+	        int round = 0;
+	        int test;
+
+	        System.out.println("Bienvenue dans le jeu du nombre mystère !");
+	        System.out.println("Devinez le nombre entre 1 et 100 :");
+
+
+	}
+
+}
